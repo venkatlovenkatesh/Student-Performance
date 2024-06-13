bf70ee5171f91121fefa651fd8ed92593cc14c05
Venkatesh J <venkatlovenkatesh@gmail.com>
2024-06-13 12:40:51 +0530

    hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -21,6 +21,7 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
@@ -52,3 +53,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e, sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
